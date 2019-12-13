09cd54afbbf652f5c13437a8d98d722a986705f2
Akshay123-java <akshaychidre1995@gmail.com>
2019-12-13 23:35:51 +0530

    First Project

---
+++ src/com/test/Test_Class.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class Test_Class {
+
+	public Test_Class() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static int i =10;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" print static variable :"+i);
+		
+		
+		
+	}
+
+}
